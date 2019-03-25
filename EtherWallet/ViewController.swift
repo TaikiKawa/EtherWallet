876501756eadce0ed7c20ffd8ae2be8205141678
taiki <nikora0425@icloud.com>
2019-03-25 17:05:41 +0900

    First

---
+++ EtherWallet/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EtherWallet
+//
+//  Created by taiki on 2019/03/25.
+//  Copyright © 2019 taiki. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+
+
+}
+
