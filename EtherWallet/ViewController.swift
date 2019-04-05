e05abde6e4938cc98fb0b6580ff1d9b87703d60e
taiki <nikora0425@icloud.com>
2019-04-05 15:04:11 +0900

    update前

--- EtherWallet/ViewController.swift
+++ EtherWallet/ViewController.swift
@@ -1,38 +1,14 @@
-//
-//  ViewController.swift
-//  EtherWallet
-//
-//  Created by taiki on 2019/03/25.
-//  Copyright © 2019 taiki. All rights reserved.
-//
-
 import UIKit
 import EthereumKit
-import CryptoSwift
 
 class ViewController: UIViewController {
     
-    @IBOutlet var AddressLabel: UILabel!
-    @IBOutlet var balanceLabel: UILabel!
-    
-//    func something() -> (balance:Int?, error:Error?) {
-//        return (0, nil)
-//    }
-//
-//    func test() {
-//        let (balance, error) = something()
-//        if let balance = balance {
-//            print(balance)
-//        }
-//
-//        if let error
-//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // It generates an array of random mnemonic words. Use it for back-ups.
         // You can specify which language to use for the sentence by second parameter.
-        let mnemonic = Mnemonic.create(entropy: Data(hex: "000102030405060708090a0bcc0d0e0f"))
+        let mnemonic = Mnemonic.create(entropy: Data(hex: "044102030405060708090a0b0c0d0e0f"))
         
         // Then generate seed data from the mnemonic sentence.
         // You can set password for more secure seed data.
@@ -50,7 +26,6 @@ class ViewController: UIViewController {
         // Generate an address, or private key by simply calling
         let address = wallet.address()
         
-        AddressLabel.text = address
         // Create an instance of `Geth` with `Configuration`.
         // In configuration, specify
         // - network: network to use
@@ -58,31 +33,16 @@ class ViewController: UIViewController {
         // - etherscanAPIKey: api key of etherscan
         let configuration = Configuration(
             network: .ropsten,
-            nodeEndpoint: "https://ropsten.infura.io/v3/8b7da192e2434a80b6dfc2fcffe05a78",
+            nodeEndpoint: "https://ropsten.infura.io/z1sEfnzz0LLMsdYMX4PV",
             etherscanAPIKey: "XE7QVJNVMKJT75ATEPY1HPWTPYCVCKMMJ7",
             debugPrints: true
         )
         
-      
-        
         let geth = Geth(configuration: configuration)
         
         // To get a balance of an address, call `getBalance`.
-        geth.getBalance(of: address) { result in
-            // Do something
-//
-            switch result {
-            case .success(let obj):
-                print(obj.wei)
-            case .failure(let error):
-                print(error)
-            }
-            
-            let object = "なんでもいいん"
-            
-            
-            print("ここまで")
-        }
+        geth.getBalance(of: address) { _ in }
+        
         // You can get the current nonce by calling
         geth.getTransactionCount(of: address) { result in
             switch result {
@@ -91,34 +51,35 @@ class ViewController: UIViewController {
                 do {
                     wei = try Converter.toWei(ether: "0.00001")
                 } catch let error {
-                    fatalError("Error1: \(error.localizedDescription)")
+                    fatalError("Error: \(error.localizedDescription)")
                 }
                 
-                let rawTransaction = RawTransaction(value: 100000, to: "0x5Dd19Cd92119E74395701f2F141884608E47607e", gasPrice: Converter.toWei(GWei: 10), gasLimit: 21000, nonce: nonce )
+//                let rawTransaction = RawTransaction(value: wei, to: address, gasPrice: Converter.toWei(GWei: 10), gasLimit: 21000, nonce: nonce)
+//
+                let str = "☺️😛😇🤑😎"
+                let data: Data? = str.data(using: .utf8)
+                
+                
+//                let data = Data(hex: "0404")
+                
+                
+                let rawTransaction = RawTransaction(wei: "100", to: address, gasPrice: Converter.toWei(GWei: 10), gasLimit: 121000, nonce: nonce, data: data!)
+                
                 let tx: String
                 do {
                     tx = try wallet.sign(rawTransaction: rawTransaction)
                 } catch let error {
-                    fatalError("Error2: \(error.localizedDescription)")
+                    fatalError("Error: \(error.localizedDescription)")
                 }
                 
                 // It returns the transaction ID.
                 geth.sendRawTransaction(rawTransaction: tx) { _ in }
                 
             case .failure(let error):
-                print("Error3: \(error.localizedDescription)")
+                print("Error: \(error.localizedDescription)")
             }
         }
         
-//        let contract = ERC20(contractAddress: "0xd99b8A7fA48E25Cce83B81812220A3E03Bf64e5f", decimal: 18, symbol: "SKM")
-//        geth.getTokenBalance(contract: contract, address: address) { result in
-//            switch result {
-//
-//            case .success(let balance):
-//                print("Token balance: \(balance)")
-//            case .failure(let error):
-//                print("Error: \(error.localizedDescription)")
-//            }
-//        }
+        let contract = ERC20(contractAddress: "0xd99b8A7fA48E25Cce83B81812220A3E03Bf64e5f", decimal: 18, symbol: "SKM")
     }
 }
